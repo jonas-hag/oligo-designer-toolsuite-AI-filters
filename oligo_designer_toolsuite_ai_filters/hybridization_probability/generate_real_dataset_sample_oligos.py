20a04f4915a0d5732f0ed41cff49bd4921ebb79f
Jonas Hagenberg <38041374+jonas-hag@users.noreply.github.com>
2025-10-22 16:18:20 +0200

    correct type in argument

--- oligo_designer_toolsuite_ai_filters/hybridization_probability/generate_real_dataset_sample_oligos.py
+++ oligo_designer_toolsuite_ai_filters/hybridization_probability/generate_real_dataset_sample_oligos.py
@@ -91,7 +91,7 @@ def sample_oligos(oligo_fasta_file, oligo_fasta_file_length, sample_information,
             logger.warning(f"The interval {interval_data['lower']} >= x <= {interval_data['upper']} "\
                            f"only has {temp_data.shape[0]} oligos instead of {interval_data['n']}")
             information_fewer_oligos.append([interval_data['lower'], interval_data['upper'],
-                                             interval_data['ln'], temp_data.shape[0]])
+                                             interval_data['n'], temp_data.shape[0]])
             sampled_data = temp_data
         else:
             sampled_data = temp_data.sample(n=interval_data["n"], random_state=seed)
